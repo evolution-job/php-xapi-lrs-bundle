f75edf30ebb18e91a74b999a6de895e39f16e623
mathieu.boldo <mathieu.boldo@entrili.com>
2024-02-23 16:16:37 +0100

    Fix serializer for State Controllers

--- src/EventListener/SerializerListener.php
+++ src/EventListener/SerializerListener.php
@@ -4,8 +4,9 @@ namespace XApi\LrsBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Serializer\Exception\ExceptionInterface as BaseSerializerException;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Xabbuh\XApi\Serializer\StatementSerializerInterface;
+use Xabbuh\XApi\Serializer\StateSerializerInterface;
 
 /**
  * @author Christian Flothmann <christian.flothmann@xabbuh.de>
@@ -13,10 +14,14 @@ use Xabbuh\XApi\Serializer\StatementSerializerInterface;
 class SerializerListener
 {
     private $statementSerializer;
+    private $stateSerializer;
 
-    public function __construct(StatementSerializerInterface $statementSerializer)
-    {
+    public function __construct(
+        StatementSerializerInterface $statementSerializer,
+        StateSerializerInterface $stateSerializer
+    ) {
         $this->statementSerializer = $statementSerializer;
+        $this->stateSerializer = $stateSerializer;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
@@ -29,11 +34,15 @@ class SerializerListener
 
         try {
             switch ($request->attributes->get('xapi_serializer')) {
+
+                case 'state':
+                    $request->attributes->set('state', $this->stateSerializer->deserializeState($request->query->all(), $request->getContent()));
+                    break;
                 case 'statement':
                     $request->attributes->set('statement', $this->statementSerializer->deserializeStatement($request->getContent()));
                     break;
             }
-        } catch (BaseSerializerException $e) {
+        } catch (ExceptionInterface $e) {
             throw new BadRequestHttpException(sprintf('The content of the request cannot be deserialized into a valid xAPI %s.', $request->attributes->get('xapi_serializer')), $e);
         }
     }
